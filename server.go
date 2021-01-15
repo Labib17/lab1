f1856c383d6f476113b078adbd27a401acb741a0
Labib17 <42694482+Labib17@users.noreply.github.com>
2021-01-15 11:12:51 +0200

    Update server.go

--- server.go
+++ server.go
@@ -1,70 +1,24 @@
 package main
-
 import (
-  "encoding/json"
-  "html/template"
-  "net/http"
-  "path"
   "time"
+  "encoding/json"
+  "log"
+  "net/http"
 )
 
-//Time ...
-type Time struct {
-  Hour    int `json:"hour"`
-  Minutes int `json:"min"`
-  Second  int `json:"sec"`
-}
-
 func main() {
-  http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+  type Time struct{
+    Time string `json:"time"`
+  }
 
-  http.HandleFunc("/", mainPage)
-  http.HandleFunc("/online", online)
-  http.HandleFunc("/time", timePage)
-  http.ListenAndServe(":8795", nil)
-}
+  http.HandleFunc("/time", func(w http.ResponseWriter, req *http.Request) {
+    test := &Time{Time: time.Now().Format(time.RFC3339)} //current time
+    res, _ := json.Marshal(test)
+    w.Header().Set("content-type", "aplication/json")
+    w.WriteHeader(200)
+    w.Write(res)
+  })
 
-func timePage(w http.ResponseWriter, r *http.Request) {
-  p := time.Now()
-  time := Time{p.Hour(), p.Minute(), p.Second()}
-  JSON, _ := json.Marshal(time)
-  fp := path.Join("server-time.html")
-  tmpl, err := template.ParseFiles(fp)
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
-  }
-  if err := tmpl.Execute(w, time); err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-  }
-  w.Write([]byte("<ul class=" + "list-group list-group-horizontal" + ">"))
-  w.Write([]byte("<li class=" + "list-group-item" + ">"))
-  w.Write([]byte(JSON))
-  w.Write([]byte("</li>"))
-  w.Write([]byte("</ul>"))
-}
-func mainPage(w http.ResponseWriter, r *http.Request) {
-  fp := path.Join("main.html")
-  tmpl, err := template.ParseFiles(fp)
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
-  }
-  if err := tmpl.Execute(w, nil); err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-  }
-}
-
-func online(w http.ResponseWriter, r *http.Request) {
-  p := time.Now()
-  time := Time{p.Hour(), p.Minute(), p.Second()}
-  fp := path.Join("online-time.html")
-  tmpl, err := template.ParseFiles(fp)
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
-  }
-  if err := tmpl.Execute(w, time); err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-  }
+  log.Println("Starting HTTP server")
+  log.Fatal(http.ListenAndServe(":8795", nil))  //http://localhost:8795/time
 }
